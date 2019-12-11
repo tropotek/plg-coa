2e4a600af9e377cacfea1c146bea401648585ebd
mifsudm <mifsudm@unimelb.edu.au>
2019-12-12 07:23:18 +1100

    ~Auto: Commit

---
+++ sql/00001.sql
@@ -0,0 +1,14 @@
+-- --------------------------------------------
+-- Author: Michael Mifsud <info@tropotek.com>
+-- --------------------------------------------
+--
+--
+-- --------------------------------------------
+
+
+alter table coa change profile_id course_id int unsigned default 0 not null;
+drop index profile_id on coa;
+create index course_id on coa (course_id);
+
+
+
